11c27408f465b5047d6651ccec78619feba2fbfb
Yusuf Aytas <yusufaytas@gmail.com>
2016-11-23 14:52:15 +0000

    Added ThreeSum.

---
+++ src/main/java/com/yusufaytas/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,51 @@
+package com.yusufaytas.leetcode;
+
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+ */
+public class LongestCommonPrefix
+{
+    public String longestCommonPrefix(String[] strs)
+    {
+        if (strs == null || strs.length == 0)
+        {
+            return "";
+        }
+        int current = 0;
+        boolean stop = false;
+        StringBuilder builder = new StringBuilder();
+        while (!stop)
+        {
+            Character c = null;
+            for (int i = 0; i < strs.length; i++)
+            {
+                if (strs[i] != null && strs[i].length() > current)
+                {
+                    if (c == null)
+                    {
+                        c = strs[i].charAt(current);
+                    }
+                    if (c != strs[i].charAt(current))
+                    {
+                        stop = true;
+                    }
+                }
+                else
+                {
+                    stop = true;
+                }
+            }
+            if (!stop)
+            {
+                builder.append(c);
+                current++;
+            }
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(new String[]{}));
+    }
+}
---
+++ src/main/java/com/yusufaytas/leetcode/ThreeSum.java
@@ -0,0 +1,53 @@
+package com.yusufaytas.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+ */
+public class ThreeSum
+{
+    public List<List<Integer>> threeSum(int[] nums)
+    {
+        Arrays.sort(nums);
+        List<List<Integer>> all = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length && nums[i] + nums[j] <= 0; j++)
+            {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                {
+                    continue;
+                }
+                int k = j + 1;
+                while (k < nums.length && 0 > nums[i] + nums[j] + nums[k])
+                {
+                    k++;
+                }
+                if (k < nums.length && nums[i] + nums[j] + nums[k] == 0)
+                {
+                    all.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                }
+            }
+        }
+        return all;
+    }
+
+}
