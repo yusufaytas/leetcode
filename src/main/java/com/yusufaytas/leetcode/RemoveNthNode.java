0127d60f77ebec4eaa3d81902d624c8788258af3
Yusuf Aytas <yusufaytas@gmail.com>
2016-11-23 23:26:54 +0000

    Added valid parantheses.

---
+++ src/main/java/com/yusufaytas/leetcode/RemoveNthNode.java
@@ -0,0 +1,55 @@
+package com.yusufaytas.leetcode;
+
+/*
+Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+Given n will always be valid.
+Try to do this in one pass.
+ */
+public class RemoveNthNode
+{
+    public ListNode removeNthFromEnd(ListNode head, int n)
+    {
+        ListNode nth = head, temp = head;
+        for (int i = 0; i < n; i++)
+        {
+            nth = nth.next;
+        }
+        while (nth != null && nth.next != null)
+        {
+            temp = temp.next;
+            nth = nth.next;
+        }
+        if(temp.next == null)
+        {
+            return null;
+        }
+        if(nth == null)
+        {
+            head = temp.next;
+        }
+        else
+        {
+            temp.next = temp.next.next;
+        }
+        return head;
+    }
+
+    public static class ListNode
+    {
+        int val;
+        ListNode next;
+
+        ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+}
---
+++ src/main/java/com/yusufaytas/leetcode/ValidParentheses.java
@@ -0,0 +1,43 @@
+package com.yusufaytas.leetcode;
+
+import java.util.Stack;
+
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ */
+public class ValidParentheses
+{
+    public boolean isValid(String s)
+    {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (stack.isEmpty())
+            {
+                stack.push(s.charAt(i));
+            }
+            else
+            {
+                char c = stack.peek();
+                if ((c == '(' && s.charAt(i) == ')') ||
+                        (c == '{' && s.charAt(i) == '}') ||
+                        (c == '[' && s.charAt(i) == ']'))
+                {
+                    stack.pop();
+                }
+                else
+                {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println(new ValidParentheses().isValid("{()[]{}}"));
+    }
+}
