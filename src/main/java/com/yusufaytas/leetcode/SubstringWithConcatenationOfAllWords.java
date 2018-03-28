855223a3a34e1cd73b0590f751b85bb8e36500fc
Yusuf Aytas <yusufaytas@gmail.com>
2018-03-28 21:58:11 +0100

    Added solution for SubstringWithConcatenationOfAllWords TLE

---
+++ src/main/java/com/yusufaytas/leetcode/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,79 @@
+package com.yusufaytas.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+You are given a string, s, and a list of words, words, that are all of the same length.
+Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once
+and without any intervening characters.
+
+For example, given:
+s: "barfoothefoobarman"
+words: ["foo", "bar"]
+
+You should return the indices: [0,9].
+(order does not matter).
+ */
+public class SubstringWithConcatenationOfAllWords
+{
+    public List<Integer> findSubstring(String s, String[] words)
+    {
+        if (s == null || s.isEmpty() || words == null || words.length == 0)
+        {
+            return Collections.emptyList();
+        }
+        int size = words[0].length() * words.length;
+        List<Integer> indexes = new ArrayList<>();
+        Set<String> permutations = getPermutations(words);
+
+        for (int i = 0; i < s.length() - size + 1; i++)
+        {
+            String sub = s.substring(i, i + size);
+            if (permutations.contains(sub))
+            {
+                indexes.add(i);
+            }
+        }
+        return indexes;
+    }
+
+    private Set<String> getPermutations(String[] words)
+    {
+        Set<String> permutations = new HashSet<>();
+        findPermutations("", Arrays.asList(words), permutations);
+        return permutations;
+    }
+
+    private void findPermutations(String s, List<String> ts, Set<String> permutations)
+    {
+        if (ts.isEmpty())
+        {
+            permutations.add(s);
+        }
+        for (int i = 0; i < ts.size(); i++)
+        {
+            String newPermutation = s + ts.get(i);
+            List<String> newList = new ArrayList<>(ts.size() - 1);
+            for (int j = 0; j < ts.size(); j++)
+            {
+                if (i != j)
+                {
+                    newList.add(ts.get(j));
+                }
+            }
+            findPermutations(newPermutation, newList, permutations);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String s = "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel";
+        String[] words = {"dhvf", "sind", "ffsl", "yekr", "zwzq", "kpeo", "cila", "tfty", "modg", "ztjg", "ybty", "heqg", "cpwo", "gdcj", "lnle", "sefg", "vimw", "bxcb"};
+        System.out.println(new SubstringWithConcatenationOfAllWords().findSubstring(s, words));
+    }
+}
