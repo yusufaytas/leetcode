0c6363005f5c283be168b43bed6eaebec437a2ea
Yusuf Aytas <yaytas@tripadvisor.com>
2019-10-17 14:13:25 +0100

    Added DP solution for LongestIncreasingSubsequence

--- src/main/java/com/yusufaytas/leetcode/LongestIncreasingSubsequence.java
+++ src/main/java/com/yusufaytas/leetcode/LongestIncreasingSubsequence.java
@@ -1,16 +1,15 @@
 package com.yusufaytas.leetcode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 /*
 Given an unsorted array of integers, find the length of longest increasing subsequence.
 
 Example:
 
-Input: [10,9,2,5,3,4]
-Output: 3
+Input: [10, 9, 2, 5, 3, 7, 101, 18]
+
+Output: 4
 Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
 
 Note:
@@ -24,34 +23,23 @@ public class LongestIncreasingSubsequence
 {
     public int lengthOfLIS(final int[] nums)
     {
-        final List<Integer> list = new ArrayList<>();
-        return lengthOfLIS(nums, 0, list);
-    }
-
-    public int lengthOfLIS(final int[] nums, final int index, final List<Integer> list)
-    {
-        if (index == nums.length)
+        if(nums.length == 0)
         {
-            return list.size();
+            return 0;
         }
-        if (list.isEmpty() || nums[index] > list.get(list.size() - 1))
+        final int[] localMax = new int[nums.length];
+        Arrays.fill(localMax, 1);
+        for (int i = 1; i < nums.length; i++)
         {
-            list.add(nums[index]);
-            return lengthOfLIS(nums, index + 1, list);
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] > nums[j])
+                {
+                    localMax[i] = Math.max(localMax[i], localMax[j] + 1);
+                }
+            }
         }
-        if (list.size() == 1)
-        {
-            list.remove(0);
-            list.add(nums[index]);
-            return lengthOfLIS(nums, index + 1, list);
-        }
-        final int newIndex = Math.abs(Collections.binarySearch(list, nums[index]));
-        final List<Integer> newList = new ArrayList<>(newIndex);
-        for (int i = 0; i < newIndex - 1; i++)
-        {
-            newList.add(list.get(i));
-        }
-        return Math.max(lengthOfLIS(nums, index + 1, list), lengthOfLIS(nums, index, newList));
+        return Arrays.stream(localMax).max().getAsInt();
     }
 
     public static void main(String[] args)
