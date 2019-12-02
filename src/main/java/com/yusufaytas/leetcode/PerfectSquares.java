31e891e0fb0781872b933ebeebb85d01a25ce6ef
Yusuf Aytas <yusufaytas@gmail.com>
2019-12-02 13:52:15 +0000

    Added working version of perfect squares.

---
+++ src/main/java/com/yusufaytas/leetcode/PerfectSquares.java
@@ -0,0 +1,64 @@
+package com.yusufaytas.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+
+Input: n = 12
+Output: 3
+Explanation: 12 = 4 + 4 + 4.
+
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+ */
+public class PerfectSquares
+{
+    public int numSquares(final int n)
+    {
+        final List<Integer> squares = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++)
+        {
+            squares.add(i * i);
+        }
+        final int[][] sums = new int[n + 1][squares.size() + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            sums[i][0] = 0;
+        }
+        for (int i = 0; i <= squares.size(); i++)
+        {
+            sums[0][i] = 0;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= squares.size(); j++)
+            {
+                if (i - squares.get(j - 1) >= 0)
+                {
+                    sums[i][j] = sums[i - squares.get(j - 1)][j] + 1;
+                }
+                if (sums[i][j - 1] > 0)
+                {
+                    sums[i][j] = Math.min(sums[i][j], sums[i][j - 1]);
+                }
+                if(sums[i][j] == 0)
+                {
+                    sums[i][j] = sums[i][j - 1];
+                }
+            }
+        }
+        return sums[n][squares.size()];
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(new PerfectSquares().numSquares(23));
+    }
+}
