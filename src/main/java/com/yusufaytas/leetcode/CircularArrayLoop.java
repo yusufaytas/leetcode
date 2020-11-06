f7d9e0d11d266b955b948182a0a37281ae23334a
Yusuf Aytas <yusufaytas@gmail.com>
2020-11-06 12:04:23 +0000

    Added CircularArrayLoop

---
+++ src/main/java/com/yusufaytas/leetcode/CircularArrayLoop.java
@@ -0,0 +1,72 @@
+package com.yusufaytas.leetcode;
+
+/*
+You are given a circular array nums of positive and negative integers.
+ If a number k at an index is positive, then move forward k steps. Conversely, if it's negative (-k),
+ move backward k steps. Since the array is circular, you may assume that the last element's next
+ element is the first element, and the first element's previous element is the last element.
+
+Determine if there is a loop (or a cycle) in nums. A cycle must start and end at the same index
+and the cycle's length > 1. Furthermore, movements in a cycle must all follow a single direction.
+In other words, a cycle must not consist of both forward and backward movements
+
+Example 1:
+
+Input: [2,-1,1,2,2]
+Output: true
+Explanation: There is a cycle, from index 0 -> 2 -> 3 -> 0. The cycle's length is 3.
+
+Example 2:
+
+Input: [-1,2]
+Output: false
+Explanation: The movement from index 1 -> 1 -> 1 ... is not a cycle, because the cycle's length is 1.
+By definition the cycle's length must be greater than 1.
+
+Example 3:
+
+Input: [-2,1,-1,-2,-2]
+Output: false
+Explanation: The movement from index 1 -> 2 -> 1 -> ... is not a cycle, because movement
+from index 1 -> 2 is a forward movement, but movement from index 2 -> 1 is a backward movement.
+All movements in a cycle must follow a single direction.
+ */
+public class CircularArrayLoop
+{
+    public boolean circularArrayLoop(final int[] nums)
+    {
+        for (int i = 0; i < nums.length; i++)
+        {
+            int nextIndex = i;
+            int cycleLength = 0;
+            boolean isForward = nums[nextIndex] > 0;
+            final boolean[] visited = new boolean[nums.length];
+            do
+            {
+                if (nums[i] >= nums.length || visited[(nextIndex % nums.length + nums.length) % nums.length])
+                {
+                    break;
+                }
+                visited[(nextIndex % nums.length + nums.length) % nums.length] = true;
+                final int newNextIndex = nums[(nextIndex % nums.length + nums.length) % nums.length] + nextIndex;
+                if ((isForward && newNextIndex < nextIndex) || (!isForward && newNextIndex > nextIndex))
+                {
+                    break;
+                }
+                cycleLength++;
+                nextIndex = newNextIndex;
+                if ((nextIndex % nums.length + nums.length) % nums.length == i && cycleLength > 1)
+                {
+                    return true;
+                }
+            } while (!visited[(nextIndex % nums.length + nums.length) % nums.length]);
+        }
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        final int[] nums = {-1,-1,-1};
+        System.out.println(new CircularArrayLoop().circularArrayLoop(nums));
+    }
+}
