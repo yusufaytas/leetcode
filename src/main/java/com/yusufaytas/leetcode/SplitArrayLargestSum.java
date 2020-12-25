65f189be1a049ac5d87a618d82319afa26f62ed7
Yusuf Aytas <yusufaytas@gmail.com>
2020-12-25 18:52:43 +0000

    Added SplitArrayLargestSum

---
+++ src/main/java/com/yusufaytas/leetcode/SplitArrayLargestSum.java
@@ -0,0 +1,72 @@
+package com.yusufaytas.leetcode;
+
+import java.util.Arrays;
+
+/*
+Given an array nums which consists of non-negative integers and an integer m,
+you can split the array into m non-empty continuous subarrays.
+
+Write an algorithm to minimize the largest sum among these m subarrays.
+
+
+
+Example 1:
+
+Input: nums = [7,2,5,10,8], m = 2
+Output: 18
+Explanation:
+There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8],
+where the largest sum among the two subarrays is only 18.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5], m = 2
+Output: 9
+
+Example 3:
+
+Input: nums = [1,4,4], m = 3
+Output: 4
+
+ */
+public class SplitArrayLargestSum {
+
+  public int splitArray(final int[] nums, final int m) {
+    if (nums == null || nums.length < m) {
+      return -1;
+    }
+    final int[][] splits = new int[nums.length][m];
+    for (int i = 0; i < splits.length; i++) {
+      Arrays.fill(splits[i], -1);
+    }
+    return splitArray(nums, splits, 0, m - 1);
+  }
+
+  public int splitArray(final int[] nums, final int[][] splits, final int index, final int m) {
+    if (splits[index][m] != -1) {
+      return splits[index][m];
+    }
+    if (m == 0) {
+      int sum = 0;
+      for (int i = index; i < nums.length; i++) {
+        sum += nums[i];
+      }
+      splits[index][m] = sum;
+      return sum;
+    }
+    int min = Integer.MAX_VALUE, sum = 0;
+    for (int i = index; i < nums.length - m; i++) {
+      sum += nums[i];
+      min = Math.min(min, Math.max(splitArray(nums, splits, i + 1, m - 1), sum));
+    }
+    splits[index][m] = min;
+    return min;
+  }
+
+  public static void main(String[] args) {
+    final int[] nums = {1,4,4};
+    final int m = 3;
+    System.out.println(new SplitArrayLargestSum().splitArray(nums, m));
+  }
+}
