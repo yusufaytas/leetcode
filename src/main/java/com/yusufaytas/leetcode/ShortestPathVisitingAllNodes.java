32cb2763e50bd2518d316796f7b3efe6fa23761f
Yusuf Aytas <yusufaytas@gmail.com>
2020-12-27 14:50:33 +0000

    Added working solution for ShortestPathVisitingAllNodes

--- src/main/java/com/yusufaytas/leetcode/ShortestPathVisitingAllNodes.java
+++ src/main/java/com/yusufaytas/leetcode/ShortestPathVisitingAllNodes.java
@@ -1,9 +1,9 @@
 package com.yusufaytas.leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
 
 /*
 An undirected, connected graph of N nodes (labeled 0, 1, 2, ..., N-1) is given as graph.
@@ -25,6 +25,7 @@ Input: [[1],[0,2,4],[1,3,4],[2],[1,2]]
 Output: 4
 Explanation: One possible path is [0,1,4,2,3]
 
+TODO: revisit
  */
 public class ShortestPathVisitingAllNodes {
 
@@ -32,42 +33,29 @@ public class ShortestPathVisitingAllNodes {
     if (graph == null || graph.length == 0) {
       return 0;
     }
-    final Map<Integer, List<Integer>> nodes = new HashMap<>();
+    //use bit manipulation for visited
+    final int ALL_VISITED = (int) (Math.pow(2, graph.length)) - 1;
+    final Queue<int[]> queue = new ArrayDeque<>();
     for (int i = 0; i < graph.length; i++) {
-      nodes.put(i, new ArrayList<>());
-      for (int j = 0; j < graph[i].length; j++) {
-        nodes.get(i).add(graph[i][j]);
+      //the node id, bitwise visited, length
+      queue.add(new int[]{i, 1 << i, 1});
+    }
+    final Set<String> visited = new HashSet<>();
+    while (!queue.isEmpty()) {
+      final int[] visit = queue.poll();
+      final String visitKey = visit[0] + "." + visit[1];
+      if (!visited.add(visitKey)) {
+        continue;
+      }
+      if (visit[1] == ALL_VISITED) {
+        return visit[2] - 1;
+      }
+      for (int i = 0; i < graph[visit[0]].length; i++) {
+        final int neighbour = graph[visit[0]][i];
+        queue.add(new int[]{neighbour, visit[1] | (1 << neighbour), visit[2] + 1});
       }
     }
-    int min = Integer.MAX_VALUE;
-    final Map<Integer, Integer> visited = new HashMap<>();
-    for (int i = 0; i < graph.length; i++) {
-      visited.put(i, 1);
-      min = Math.min(shortestPathLength(nodes, visited, i), min);
-      visited.remove(i);
-    }
-    return min - 1;
-  }
-
-  public int shortestPathLength(final Map<Integer, List<Integer>> nodes,
-      final Map<Integer, Integer> visited, final int current) {
-    if (visited.size() == nodes.size()) {
-      return visited.values().stream().mapToInt(value -> value).sum();
-    }
-    int min = Integer.MAX_VALUE;
-    final List<Integer> neighbours = nodes.get(current);
-    for (int i = 0; i < neighbours.size(); i++) {
-      final int neighbour = neighbours.remove(i);
-      visited.put(neighbour, visited.getOrDefault(neighbour, 0) + 1);
-      min = Math.min(shortestPathLength(nodes, visited, neighbour), min);
-      neighbours.add(i, neighbour);
-      if (visited.get(neighbour) == 1) {
-        visited.remove(neighbour);
-      } else {
-        visited.put(neighbour, visited.get(neighbour) - 1);
-      }
-    }
-    return min;
+    return -1;
   }
 
   public static void main(String[] args) {
