43940bd45777a900a66ce352fd604d0cff2604d9
Yusuf Aytas <yusufaytas@gmail.com>
2020-12-30 21:16:31 +0000

    NumberOfMatchingSubsequences

---
+++ src/main/java/com/yusufaytas/leetcode/NumberOfMatchingSubsequences.java
@@ -0,0 +1,65 @@
+package com.yusufaytas.leetcode;
+
+import java.util.Arrays;
+
+/*
+Given string S and a dictionary of words words, find the number of words[i] that is a subsequence of S.
+
+Example :
+Input:
+S = "abcde"
+words = ["a", "bb", "acd", "ace"]
+Output: 3
+Explanation: There are three words in words that are a subsequence of S: "a", "acd", "ace".
+
+Note:
+    All words in words and S will only consists of lowercase letters.
+    The length of S will be in the range of [1, 50000].
+    The length of words will be in the range of [1, 5000].
+    The length of words[i] will be in the range of [1, 50].
+ */
+public class NumberOfMatchingSubsequences {
+
+  public int numMatchingSubseq(final String S, final String[] words) {
+    if (S == null || S.length() == 0 || words == null || words.length == 0) {
+      return 0;
+    }
+    final int[][] letterCount = new int[26][S.length()];
+    for (int i = 0; i < 26; i++) {
+      Arrays.fill(letterCount[i], -1);
+    }
+    letterCount[S.charAt(S.length() - 1) - 'a'][S.length() - 1] = S.length() - 1;
+    for (int i = S.length() - 2; i >= 0; i--) {
+      for (int j = 0; j < 26; j++) {
+        letterCount[j][i] = letterCount[j][i + 1];
+      }
+      letterCount[S.charAt(i) - 'a'][i] = i;
+    }
+    int matchingCount = 0;
+    for (int i = 0; i < words.length; i++) {
+      int current = 0;
+      for (int j = 0; j < words[i].length(); j++) {
+        if (current >= S.length() || letterCount[words[i].charAt(j) - 'a'][current] < 0) {
+          matchingCount--;
+          break;
+        }
+        current = letterCount[words[i].charAt(j) - 'a'][current] + 1;
+      }
+      matchingCount++;
+    }
+    return matchingCount;
+  }
+
+  public static void main(String[] args) {
+    final String S = "iwdlcxpyagegrcnrcylxolxlnhhwnxyzltiscrjztiivnpnzlubzpueihinsqdfvypdteztiodbhaqhxskupwulvkzhczdyoouym";
+    final String[] words = {"hhwnxyzltiscrjztiivnpnzlubzpueihinsqdfvyp",
+        "vnpnzlubzpueihinsqdfvypdteztiodbha", "rcnrcylxolxlnhhwnxyzltiscrjztiivnpnzlubzpueihi",
+        "dfvypdteztiodbhaqhxskupwulvk", "zltiscrjztii",
+        "wdmbatbcewwittubryrqwwrvfkrmniomofygybeqfzusrgeart",
+        "myzfexqmzxnbmmnhmpbddqhrwrobqzjiwdzzpyzodejysuuquc",
+        "wxvrcbihbasohfvuwuxleesqeujxvjfvgwnhltenbspdgzsdrs",
+        "nztyysfhfbfcihyeaqdarqxfpjunevabzafvbmpbtenarvyizv",
+        "nivufheyodfjuggrbndyojeahrzgptikjfqufhwyhzyyjteahx"};
+    System.out.println(new NumberOfMatchingSubsequences().numMatchingSubseq(S, words));
+  }
+}
