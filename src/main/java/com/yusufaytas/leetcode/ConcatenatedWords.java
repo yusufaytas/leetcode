ba45a7b42de1ad621112c8253089f321ced60b45
Yusuf Aytas <yusufaytas@gmail.com>
2021-01-30 14:10:21 +0000

    ConcatenatedWords

---
+++ src/main/java/com/yusufaytas/leetcode/ConcatenatedWords.java
@@ -0,0 +1,76 @@
+package com.yusufaytas.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/*
+Given an array of strings words (without duplicates), return all the concatenated words in the given list of words.
+A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+
+Example 1:
+
+Input: words = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats";
+"dogcatsdog" can be concatenated by "dog", "cats" and "dog";
+"ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+
+Example 2:
+
+Input: words = ["cat","dog","catdog"]
+Output: ["catdog"]
+ */
+public class ConcatenatedWords {
+
+  public List<String> findAllConcatenatedWordsInADict(final String[] words) {
+    if (words == null || words.length == 0) {
+      return Collections.emptyList();
+    }
+    Arrays.sort(words);
+    final Set<String> wordSet = Stream.of(words).filter(w -> !w.isEmpty())
+        .collect(Collectors.toSet());
+    final List<String> concatenatedWords = new ArrayList<>();
+    final Map<String, Boolean> visited = new HashMap<>();
+    for (final String word : words) {
+      if (!word.isEmpty() && isConcatenatedWord(visited, wordSet, word, 0)) {
+        concatenatedWords.add(word);
+      }
+    }
+    return concatenatedWords;
+  }
+
+  public boolean isConcatenatedWord(final Map<String, Boolean> visited, final Set<String> words,
+      final String word, final int index) {
+    if (index == word.length()) {
+      return true;
+    }
+    if (visited.containsKey(word.substring(index)) && index > 0) {
+      return visited.get(word.substring(index));
+    }
+    String temp = "";
+    for (int i = index; i < word.length(); i++) {
+      temp += word.charAt(i);
+      if (words.contains(temp) && !temp.equals(word)
+          && isConcatenatedWord(visited, words, word, i + 1)) {
+        visited.put(word.substring(i + 1), true);
+        return true;
+      }
+    }
+    if (index > 0) {
+      visited.put(word.substring(index), false);
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    final String[] words = {"cat", "cats", "catsdogcats", "dog"};
+    System.out.println(new ConcatenatedWords().findAllConcatenatedWordsInADict(words));
+  }
+}
