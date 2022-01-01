f69a3e48d68f3058fca64c21be8aff60d1d0868d
yusufaytas <yusufaytas@gmail.com>
2022-01-01 08:41:42 +0000

    Updated solution for ProductOfArrayExceptSelf

--- src/main/java/com/yusufaytas/leetcode/ProductOfArrayExceptSelf.java
+++ src/main/java/com/yusufaytas/leetcode/ProductOfArrayExceptSelf.java
@@ -1,16 +1,14 @@
 /**
  * Copyright © 2021 Yusuf Aytas. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package com.yusufaytas.leetcode;
@@ -35,33 +33,33 @@ Note: Please solve it without division and in O(n).
 Follow up:
 Could you solve it with constant space complexity? (The output array does not count as
 extra space for the purpose of space complexity analysis.)
-TODO: revisit
+
  */
 public class ProductOfArrayExceptSelf {
 
   public static void main(String[] args) {
-    final int nums[] = {1, 2, 3, 4};
+    final int nums[] = {0, 0};
     printArray(new ProductOfArrayExceptSelf().productExceptSelf(nums));
   }
 
   public int[] productExceptSelf(final int[] nums) {
-    final int[] product = new int[nums.length];
-    if (nums == null && nums.length == 0) {
-      return product;
+    if (nums == null || nums.length == 0) {
+      return null;
     }
-    final int[] leftProduct = new int[nums.length];
-    leftProduct[0] = nums[0];
-    final int[] rightProduct = new int[nums.length];
-    rightProduct[nums.length - 1] = nums[nums.length - 1];
-    for (int i = 1; i < nums.length; i++) {
-      leftProduct[i] = nums[i] * leftProduct[i - 1];
-      rightProduct[nums.length - i - 1] = nums[nums.length - i - 1] * rightProduct[nums.length - i];
+    int left = 1, right = 1;
+    final int[] leftProducts = new int[nums.length];
+    final int[] rightProducts = new int[nums.length];
+    final int[] products = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      left *= nums[i];
+      leftProducts[i] = left;
+      right *= nums[nums.length - i - 1];
+      rightProducts[nums.length - i - 1] = right;
     }
-    for (int i = 1; i < nums.length - 1; i++) {
-      product[i] = leftProduct[i - 1] * rightProduct[i + 1];
+    for (int i = 0; i < nums.length; i++) {
+      products[i] =
+          (i > 0 ? leftProducts[i - 1] : 1) * (i < nums.length - 1 ? rightProducts[i + 1] : 1);
     }
-    product[0] = rightProduct[1];
-    product[nums.length - 1] = leftProduct[nums.length - 2];
-    return product;
+    return products;
   }
 }
